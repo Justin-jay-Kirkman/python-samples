8dbbb4f287eae421926583b4d1a7fcafde90838e
JK <justin.j.kirkman@gmail.com>
2024-05-16 23:11:56 -0400

    updates to game of life

--- game_of_life.py
+++ game_of_life.py
@@ -16,93 +16,100 @@
 #
 import copy
 
-# prints the data to screen as an X
-def print_board(board):
-    for row in board:
-        print_row = ''
-        for cell in row:
-            if cell == 1:
-                print_row += "X "
-            else:
-                print_row += "  "
-        print(print_row)
-    print("--------------")
+class GameOfLife:
+    def __init__(self, width, height, board):
+        self.width = width
+        self.height = height
+        self.board = [[0 for x in range(width)] for y in range(height)]
+        self.board = board
 
+    def print_board(self, board=None):
+        if board is None:
+            board = self.board
+        for row in board:
+            print_row = ''
+            for cell in row:
+                if cell == 1:
+                    print_row += "X "
+                else:
+                    print_row += "O "
+            print(print_row)
+        print("--------------")
 
-def next_board(board):
-    new_board = copy.deepcopy(board)
-    for x, row in enumerate(board):
-        for y, column in enumerate(row):
-            neighbors = 0
-            current_cell = board[x][y]  # added for ease of reading
+    def next_board(self, board=None):
+        if board is None:
+            board = self.board
+        new_board = copy.deepcopy(board)
+        for x, row in enumerate(board):
+            for y, column in enumerate(row):
+                neighbors = 0
+                current_cell = board[x][y]  # added for ease of reading
 
-            # Kept these separate to show logic path - though can be refactored
-            # top
-            neighbors += find_neighbor(board, x - 1, y)
-            # left
-            neighbors += find_neighbor(board, x, y - 1)
-            # right
-            neighbors += find_neighbor(board, x, y + 1)
-            # bottom
-            neighbors += find_neighbor(board, x + 1, y)
+                # Kept these separate to show logic path - though can be refactored
+                # top
+                neighbors += self.find_neighbor(board, x - 1, y)
+                # left
+                neighbors += self.find_neighbor(board, x, y - 1)
+                # right
+                neighbors += self.find_neighbor(board, x, y + 1)
+                # bottom
+                neighbors += self.find_neighbor(board, x + 1, y)
 
-            # top left
-            neighbors += find_neighbor(board, x - 1, y - 1)
-            # top right
-            neighbors += find_neighbor(board, x - 1, y + 1)
-            # bottom left
-            neighbors += find_neighbor(board, x + 1, y + 1)
-            # bottom right
-            neighbors += find_neighbor(board, x + 1, y - 1)
+                # top left
+                neighbors += self.find_neighbor(board, x - 1, y - 1)
+                # top right
+                neighbors += self.find_neighbor(board, x - 1, y + 1)
+                # bottom left
+                neighbors += self.find_neighbor(board, x + 1, y + 1)
+                # bottom right
+                neighbors += self.find_neighbor(board, x + 1, y - 1)
 
-            if current_cell == 1:
-                # 1. kill a cell
-                if neighbors < 2:
-                    new_board[x][y] = 0
-                # 2. Lives on
-                if neighbors == 2 or neighbors == 3:
+                if current_cell == 1:
+                    # 1. kill a cell
+                    if neighbors < 2:
+                        new_board[x][y] = 0
+                    # 2. Lives on
+                    if neighbors == 2 or neighbors == 3:
+                        new_board[x][y] = 1
+                    # 3. Gets starved out
+                    if neighbors > 3:
+                        new_board[x][y] = 0
+                elif neighbors == 3 and current_cell == 0:
+                    # 4. Born
                     new_board[x][y] = 1
-                # 3. Gets starved out
-                if neighbors > 3:
-                    new_board[x][y] = 0
-            elif neighbors == 3 and current_cell == 0:
-                # 4. Born
-                new_board[x][y] = 1
 
-    return new_board
+        return new_board
+
+    def find_neighbor(self, board, x, y):
+        result = 0
+        # add try exception for items larger than the list amount
+        try:
+            # prevent negative entries to not select from the right
+            if board[x][y] == 1 and x >= 0 and y >= 0:
+                result = 1
+            else:
+                result = 0
+        except Exception as e:
+            pass
+
+        return result
+
+    def run(self, limit):
+        current_board = copy.deepcopy(board)
+        for i in range(limit):
+            self.print_board(current_board)
+            current_board = self.next_board(current_board)
 
 
-def find_neighbor(board, x, y):
-    result = 0
-    # add try exception for items larger than the list amount
-    try:
-        # prevent negative entries to not select from the right
-        if board[x][y] == 1 and x >= 0 and y >= 0:
-            result = 1
-        else:
-            result = 0
-    except Exception as e:
-        pass
-
-    return result
-
-
-def game_of_life(board, limit):
-    current_board = copy.deepcopy(board)
-    for i in range(limit):
-        print_board(current_board)
-        current_board = next_board(current_board)
-
-# 1 = Alive, 0 = Dead
+# # 1 = Alive, 0 = Dead
 board = [
     [1, 0, 0],
     [1, 1, 0],
     [0, 1, 1]
 ]
 
-game_of_life(board, 10)
-
-
+Game = GameOfLife(3,3, board)
+Game.run(4)
 
 
 
